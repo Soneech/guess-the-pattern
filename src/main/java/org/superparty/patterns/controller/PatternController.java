ae6751181b48ce1e52029f613ad13254893a2156
Soneech <sonya.patrina@yandex.ru>
2023-10-11 13:21:04 +0300

    Added patch method support + Answer class

--- src/main/java/org/superparty/patterns/controller/PatternController.java
+++ src/main/java/org/superparty/patterns/controller/PatternController.java
@@ -1,10 +1,10 @@
 package org.superparty.patterns.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
 import org.superparty.patterns.service.PatternService;
 
-@RestController
+@Controller
 public class PatternController {
     private final PatternService patternService;
 
