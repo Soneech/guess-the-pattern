d454fbdd7c2684b04cd7a58aa869ee33fc7e446c
apl26 <apl2612@mail.ru>
2023-10-12 15:29:48 +0300

    findById(Long id) and getCountOfGuesses(List<Pattern> patterns) in PatternService

--- src/main/java/org/superparty/patterns/service/PatternService.java
+++ src/main/java/org/superparty/patterns/service/PatternService.java
@@ -2,7 +2,10 @@ package org.superparty.patterns.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.superparty.patterns.model.Pattern;
 import org.superparty.patterns.repository.PatternRepository;
+
+import java.util.List;
 
 @Service
 public class PatternService {
@@ -11,5 +14,13 @@ public class PatternService {
     @Autowired
     public PatternService(PatternRepository patternRepository) {
         this.patternRepository = patternRepository;
+    }
+
+    public Pattern findById(Long id) {
+        return patternRepository.findById(id).orElse(null);
+    }
+
+    public int getCountOfGuesses(List<Pattern> patterns) {
+        return patterns.stream().mapToInt(Pattern::getGuesses).sum();
     }
 }
