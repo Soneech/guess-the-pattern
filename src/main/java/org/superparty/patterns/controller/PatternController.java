446a1623b24e0db4a3c2943c91290f5970dd4296
Soneech <sonya.patrina@yandex.ru>
2023-10-12 15:44:31 +0300

    Page for pattern (by id)

--- src/main/java/org/superparty/patterns/controller/PatternController.java
+++ src/main/java/org/superparty/patterns/controller/PatternController.java
@@ -4,9 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.superparty.patterns.model.Pattern;
 import org.superparty.patterns.service.PatternService;
+import org.superparty.patterns.util.Answer;
 
 import java.util.List;
 
@@ -27,5 +29,13 @@ public class PatternController {
         model.addAttribute("patterns", patterns);
         model.addAttribute("guesses", patternService.getCountOfGuesses(patterns));
         return "patterns";
+    }
+
+    // test with cucumber
+    @GetMapping("/{id}")
+    public String patternPage(@PathVariable("id") Long id, Model model) {
+        model.addAttribute("pattern", patternService.findById(id));
+        model.addAttribute("answer", new Answer());
+        return "pattern_page";
     }
 }
